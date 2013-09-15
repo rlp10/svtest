11718a0322580f08ac7a0a7695791df53ba78b1c
Richard Parsons <richard.lee.parsons@gmail.com>
2013-09-15 10:24:54 +0100

    changed python comments to function docstrings

--- svserver.py
+++ svserver.py
@@ -5,8 +5,6 @@ import logging
 import os
 import os.path
 import pickle
-import unittest
-import tempfile
 import time
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
@@ -21,20 +19,20 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 # user.  Each key/value pair in the second dictionary is called an
 # "entry".
 
-# returns the default filename from the user's home directory as a
-# location to save data
 def default_filename():
+    '''Returns the default filename from the user's home directory as a
+    location to save data'''
     return os.path.expanduser("~/.svtest")
 
-# saves the data to disk, with a default filename if none given
 def save_data(data, filename=None):
+    '''Saves the data to disk, with a default filename if none given'''
     if filename == None:
         filename = default_filename()
     pickle.dump(data, open(filename, "wb"))
 
-# returns the loaded data from disk, with a default filename if none
-# given
 def load_data(filename=None):
+    '''Returns the loaded data from disk, using a default filename if none
+    given'''
     if filename == None:
         filename = default_filename()
     try:
@@ -46,16 +44,16 @@ def load_data(filename=None):
 # Registration #
 ################
 
-# Returns a new random token, ensuring its not already used as a
-# registration key
 def new_token(data):
+    '''Returns a new random token, ensuring it's not already used as a
+    registration key'''
     token = None
     while (token == None) or (token in data.keys()):
         token = hashlib.sha224(str(time.time())).hexdigest()
     return token
 
-# Registers a new token against the database
 def register_token(data, token):
+    '''Registers a new token against the database'''
     data[token] = {}
     return data
 
@@ -63,8 +61,8 @@ def register_token(data, token):
 # Store Data #
 ##############
 
-# Stores an entry against the relevant client's ID
 def store_entry(data, token, key, value):
+    '''Stores an entry against the relevant client's record'''
     data[token][key] = value
     return data
 
@@ -72,8 +70,8 @@ def store_entry(data, token, key, value):
 # Retrieve Data #
 #################
 
-# Retrieves an entry from the relevant client's record
 def retrieve_entry(data, token, key):
+    '''Retrieves an entry from the relevant client's record'''
     return data[token][key]
 
 ###############
@@ -83,9 +81,11 @@ def retrieve_entry(data, token, key):
 # Uses code from here: http://pymotw.com/2/SimpleXMLRPCServer/
 
 def hello():
+    '''Test function to be offered as web service'''
     return "Hello world!"
 
 def register():
+    '''Register function to be offered as web service'''
     data = load_data()
     token = new_token(data)
     data = register_token(data, token)
@@ -93,6 +93,7 @@ def register():
     return token
 
 def store(token, key, value):
+    '''Store function to be offered as web service'''
     if not (0 < len(key) < 21):
         return "Error: Key must be between 1 and 20 characters in length"
     data = load_data()
@@ -101,10 +102,12 @@ def store(token, key, value):
     return True
 
 def retrieve(token, key):
+    '''Retrieve function to be offered as web service'''
     data = load_data()
     return retrieve_entry(data,token, key)
 
 def serve_forever():
+    '''Setup web server and offer API'''
     logging.basicConfig(level = logging.DEBUG)
     server = SimpleXMLRPCServer(('localhost', 9000), logRequests=True)
     server.register_function(hello)
